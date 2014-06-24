1f6700db04dce7100e4a14c0a53317c9d5582263
maggarwal <maggarwal@amplify.com>
2014-06-24 19:32:28 -0400

    fixed a small spacing problem

--- githubCreate.sh
+++ githubCreate.sh
@@ -5,7 +5,7 @@ githubCreate() {
     username=`git config github.user`
     token=`git config github.token`
     echo "echo creating a repo $current"
-    curl -u "$username:$token" -d '{"name":"'$current'"}' https://api.github.com/user/repos> /dev/null 2>&1
+    curl -u "$username:$token" -d '{"name":"'$current'"}' https://api.github.com/user/repos > /dev/null 2>&1
 
     echo "creating remote .."
     git remote add origin git@github.com:$username/$current.git > /dev/null 2>&1
